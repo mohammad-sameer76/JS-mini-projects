18d2d7071b49f03b5634df77be04ced257e00f3e
Mohammad Sameer <mohammadsameer7651@gmail.com>
2025-10-10 10:51:33 +0530

    Added new project review section

---
+++ Review-Section/README.md
@@ -0,0 +1,38 @@
+# Review Section
+
+A simple web application that displays customer reviews in a carousel format. Users can navigate through reviews using previous and next buttons, or select a random review with the "Surprise Me" button.
+
+## Features
+
+- Display review cards with image, name, job title, and review text
+- Navigate to previous review using the "<" button
+- Navigate to next review using the ">" button
+- Randomly select a review using the "Surprise Me" button
+- Responsive design
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## How to Run
+
+
+1. Clone or download the repository.
+2. Navigate to the project folder:
+   cd JS-mini-projects/Review-Section
+3. Open the `index.html` file in a web browser.
+
+4. Interact with the review carousel using the buttons.
+
+## Screenshots
+
+![Review Screenshot](screenshot/review-ss.png)
+
+## Project Structure
+
+- `index.html`: Main HTML file
+- `style.css`: CSS styles
+- `script.js`: JavaScript logic
+- `screenshot/`: Folder containing screenshots
---
+++ Review-Section/script.js
@@ -0,0 +1,89 @@
+// local reviews data
+const reviews = [
+    {
+      id: 1,
+      name: 'susan smith',
+      job: 'web developer',
+      img: 'https://www.course-api.com/images/people/person-1.jpeg',
+      text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: 'anna johnson',
+      job: 'web designer',
+      img: 'https://www.course-api.com/images/people/person-2.jpeg',
+      text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    },
+    {
+      id: 3,
+      name: 'peter jones',
+      job: 'intern',
+      img: 'https://www.course-api.com/images/people/person-4.jpeg',
+      text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    },
+    {
+      id: 4,
+      name: 'bill anderson',
+      job: 'the boss',
+      img: 'https://www.course-api.com/images/people/person-3.jpeg',
+      text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    },
+];
+
+var author = document.getElementById("author");
+var job = document.getElementById("job");
+var info = document.getElementById("info");
+var img = document.getElementById("img");
+
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".surprise-btn");
+
+
+
+let currentItem  = 0;
+
+
+
+window.addEventListener("DOMContentLoaded",function(){
+    showPerson(currentItem);
+});
+
+function showPerson(person){
+
+    const data = reviews[person];
+    img.src = data.img;
+    author.innerText = data.name;
+    job.innerText = data.job;
+    info.innerText = data.text
+}
+
+
+
+//show next
+prevBtn.addEventListener("click",function(){
+
+  currentItem--;
+  if (currentItem < 0){
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+})
+
+//show previous
+nextBtn.addEventListener("click",function(){
+    currentItem++;
+  if (currentItem > reviews.length-1){
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+})
+
+
+// showrandom
+
+randomBtn.addEventListener("click",function(){
+  currentItem = Math.floor(Math.random()*reviews.length);
+  showPerson(currentItem);
+})
